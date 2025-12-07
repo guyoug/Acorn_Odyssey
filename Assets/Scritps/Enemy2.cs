ac2715f446c2c0f1203901e52a4dcd907e323149
guyoug <dfghj0420@naver.com>
2025-12-08 00:29:40 +0900

    2스테이지 적 구현

--- Assets/Scritps/Enemy2.cs
+++ Assets/Scritps/Enemy2.cs
@@ -4,21 +4,71 @@ public class Enemy2 : MonoBehaviour
 {
     [Header("Status")]
     public int maxspeed = 15;
- 
+    private bool isDead = false;
+
+    [Header("Drop Rates")]
+    private float dropItem = 0.05f;  // 5%
+    private float dropGauge = 0.10f; // 10%
+
+    [Header("Lifetime Settings")]
+    private float deleteTime = 10.0f;
+
+    [Header("Prefabs & Items")]
+    public GameObject[] dropItems;
+    public GameObject gaugePrefabs;
+    [Header("References")]
+    private GameManager gameManager;
+    private void Start()
+    {
+        Destroy(gameObject, deleteTime);
+    }
     void FixedUpdate()
     {
         transform.Translate(Vector3.left * maxspeed * Time.fixedDeltaTime);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (collision.CompareTag("Bullet"))
-        //{
-        //    Die();
-        //}
+        if (collision.CompareTag("Bullet"))
+        {
+            Die();
+        }
         if (collision.CompareTag("Outline"))
         {
             Destroy(gameObject);
         }
+    }
+    public void Die()
+    {
+        if (isDead)
+            return;
+        isDead = true;
+        gameManager.OnNormalEnemyKilled(); // 킬 계산
+        TryDropItem(); // 속성 아이템
+        TryDropGagueItem();// 게이지 아이템
+        Destroy(gameObject);
+    }
+    private void TryDropItem()
+    {
+        if (dropItems == null)
+        {
+            Debug.Log("dropItems 배열이 null입니다.");
+            return;
+        }
+        if (Random.value <= dropItem) //5퍼
+        {
+            int idx = Random.Range(0, dropItems.Length);
+            Instantiate(dropItems[idx], transform.position, Quaternion.identity);
+        }
+    }
+    private void TryDropGagueItem()
+    {
+        if (gaugePrefabs == null)
+        {
+            Debug.Log("gaugePrefabs가 null입니다.");
+            return;
+        }
+        if (Random.value <= dropGauge) //25퍼
+            Instantiate(gaugePrefabs, transform.position, Quaternion.identity);
     }
 
 }
